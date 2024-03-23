46a52e0699e87aabe05f74787505dcde24351330
Devennn1010 <148888282+Devennn1010@users.noreply.github.com>
2024-03-24 03:10:11 +0800

    Update app.js
    Signed-off-by: Devennn1010 <148888282+Devennn1010@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
         var Camera_Button = document.getElementById('Camera_Button');
         var Mute_Button = document.getElementById('Mute_Button');
         var Translate_Button = document.getElementById('Translate_Button');
-        const apiKey = 'AIzaSyB144WQqPzNPLeghXUstDGXbVsYz4saeg4';//API_translate
         let dataConnection; 
 
         peer.on('open', () => {
